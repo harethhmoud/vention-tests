83247408b2693bf5cf0d3b5efae0f84f7da7f46d
harethhmoud <hareth.hmoud@gmail.com>
2024-12-15 14:25:15 +0300

    Fixed French-English issues.

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -5,7 +5,8 @@ test.describe('Vention Homepage', () => {
     await page.goto('https://www.vention.io/');
 
     const heroText = await page.textContent('h1');
-    expect(heroText).toContain('Automatisation industrielle rapide et intuitive pour tous les fabricants');
+    //expect(heroText).toContain('Automatisation industrielle rapide et intuitive pour tous les fabricants');
+    expect(heroText).toContain('Fast & intuitive industrial automation for all manufacturers');
 
     const startNowButton = page.locator('[data-analytic-id="home-header-start-building-button"]');
     await expect(startNowButton).toBeVisible();
@@ -16,6 +17,7 @@ test.describe('Vention Homepage', () => {
     expect(page.url()).toContain('/signup');
 
     // checking if sign up elements are visible
-    await expect(page.locator('button:has-text("inscrire")')).toBeVisible();
+    //await expect(page.locator('button:has-text("inscrire")')).toBeVisible();
+    await expect(page.locator('button:has-text("Sign up")')).toBeVisible();
   });
 });
